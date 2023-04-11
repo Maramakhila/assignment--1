9d1c7cce4e7337322792423066947445d31ce2d5
Maramakhila <130456749+Maramakhila@users.noreply.github.com>
2023-04-11 21:21:08 +0530

    Add files via upload

---
+++ assign10..c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main() {
+    int decimal, binary = 0, base = 1;
+
+    printf("Enter a decimal number: ");
+    scanf("%d", &decimal);
+
+    while (decimal > 0) {
+        binary += (decimal % 2) * base;
+        decimal /= 2;
+        base *= 10;
+    }
+
+    printf("Binary equivalent: %d", binary);
+
+    return 0;
+}
